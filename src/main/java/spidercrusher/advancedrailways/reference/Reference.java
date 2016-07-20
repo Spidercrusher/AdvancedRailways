18a511ded67166b1b0aa292e4b0801e7d8182791
Spidercrusher <spidercrusher@gmail.com>
2016-07-20 11:04:42 +0200

    Updated to Forge 1.10.2-12.18.1.2014

--- src/main/java/spidercrusher/advancedrailways/reference/Reference.java
+++ src/main/java/spidercrusher/advancedrailways/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
     public static final String MOD_NAME = "Advanced Railways";
     // public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES = "required-after:Forge@[11.15.1,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[12.18.1,)";
     public static final String SERVER_PROXY_CLASS = "spidercrusher.advancedrailways.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "spidercrusher.advancedrailways.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "spidercrusher.advancedrailways.client.gui.config.GuiFactory";
